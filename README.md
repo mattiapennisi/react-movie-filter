6ca1e3d5670ee1d363686983d87dc83c6b163905
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-27 15:05:25 +0100

    Implement dynamic movie filtering and add movie IDs to data

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-Dovrete utilizzare lo stato e useEffect per gestire il filtraggio dinamico.
-Per oggi diamo priorità alla logica e alla gestione dello stato. Una volta funzionante, possiamo pensare allo stile!
+Dovrete utilizzare lo stato e useEffect per gestire il filtraggio dinamico
 Note
 Il filtro deve funzionare dinamicamente quando l'utente seleziona un genere dalla select.
-Se non viene selezionato alcun genere, devono essere mostrati tutti i film. :urlo:
+Se non viene selezionato alcun genere, devono essere mostrati tutti i film
 BONUS:
 Aggiungere un campo di ricerca per filtrare i film anche per titolo.
 Creare un sistema per aggiungere nuovi film alla lista tramite un form.
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,29 @@
+import { useState, useEffect } from 'react'
+
+import movies from '../../data/movies.js'
+
 export default function Main() {
     return (
-        <main id="main">
+        <main id="main" className='text-center'>
+            <h1 className='m-1 fw-bold'>Movies Database</h1>
+
+            <ul>
+                {
+                    movies.map(movie => {
+                        return <li key={movie.id} className='m-1'>{movie.title}</li>
+                    })
+                }
+            </ul>
+
+            <form>
+                <select name="moviesSelect" id="moviesSelect" className='m-1'>
+                    {
+                        movies.map(movie => {
+                            return <option>{movie.genre}</option>
+                        })
+                    }
+                </select>
+            </form>
         </main>
     )
 }
