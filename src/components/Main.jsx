972309a9200b6c5c6588527e4c42825b7c763899
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-27 17:38:13 +0100

    Add title search functionality and improve movie filtering logic

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,25 +5,22 @@ import movies from '../../data/movies.js'
 export default function Main() {
 
     const [genreSelected, setGenreSelected] = useState('')
+    const [titleSelected, setTitleSelected] = useState('')
     const [filteredMovies, setFilteredMovies] = useState(movies)
     const [addMovie, setAddMovie] = useState('')
 
     useEffect(() => {
-        const filtered = genreSelected === '' ? movies : movies.filter(movie => movie.genre === genreSelected)
-        setFilteredMovies(filtered)
-    }, [genreSelected])
+        const filteredByGenre = genreSelected === '' ? movies : movies.filter(movie => movie.genre === genreSelected)
+        const filteredByTitle = titleSelected === '' ? filteredByGenre : filteredByGenre.filter(movie => movie.title.toLowerCase().includes(titleSelected.toLowerCase()))
+        setFilteredMovies(filteredByTitle)
+    }, [genreSelected, titleSelected])
 
     return (
         <main id="main" className='text-center'>
             <h1 className='m-1 fw-bold'>Movies Database</h1>
 
-            <ul>
-                {filteredMovies.map(movie => (
-                    <li key={movie.id} className='m-1'>{movie.title}</li>
-                ))}
-            </ul>
-
             <form>
+                <h2>Filtri ricerca</h2>
                 <select name="moviesSelect" id="moviesSelect" className='m-1' value={genreSelected} onChange={(e) => setGenreSelected(e.target.value)}>
                     <option value="">Seleziona il genere</option>
                     <option value="Azione">Azione</option>
@@ -32,9 +29,14 @@ export default function Main() {
                     <option value="Fantascienza">Fantascienza</option>
                 </select>
 
-                <input type="text" placeholder='Cerca per titolo' className='m-1' />
-                <button type="submit">Invia</button>
+                <input type="text" placeholder='Cerca per titolo' className='m-1' value={titleSelected} onChange={(e) => setTitleSelected(e.target.value)} />
             </form>
+
+            <ul>
+                {filteredMovies.map(movie => (
+                    <li key={movie.id} className='m-1'>{movie.title}</li>
+                ))}
+            </ul>
 
             <form>
                 <input type="text" placeholder='Aggiungi nuovo film' className='m-1' />
