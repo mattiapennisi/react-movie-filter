38aa8d332b16ef5ec4dc61b6ca81d33afcd8378f
mattiapennisi <mattiapennisi0@gmail.com>
2025-03-27 15:33:53 +0100

    Add genre selection for movie filtering in Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,25 +3,33 @@ import { useState, useEffect } from 'react'
 import movies from '../../data/movies.js'
 
 export default function Main() {
+
+    const [genreSelected, setGenreSelected] = useState('')
+
     return (
         <main id="main" className='text-center'>
             <h1 className='m-1 fw-bold'>Movies Database</h1>
 
             <ul>
                 {
-                    movies.map(movie => {
-                        return <li key={movie.id} className='m-1'>{movie.title}</li>
-                    })
+                    genreSelected === ''
+                        ? movies.map(movie => (
+                            <li key={movie.id} className='m-1'>{movie.title}</li>
+                        ))
+                        : movies.filter(movie => movie.genre === genreSelected)
+                            .map(movie => (
+                                <li key={movie.id} className='m-1'>{movie.title}</li>
+                            ))
                 }
             </ul>
 
             <form>
-                <select name="moviesSelect" id="moviesSelect" className='m-1'>
-                    {
-                        movies.map(movie => {
-                            return <option>{movie.genre}</option>
-                        })
-                    }
+                <select name="moviesSelect" id="moviesSelect" className='m-1' value={genreSelected} onChange={(e) => setGenreSelected(e.target.value)}>
+                    <option value="">Seleziona il genere</option>
+                    <option value="Azione">Azione</option>
+                    <option value="Romantico">Romantico</option>
+                    <option value="Thriller">Thriller</option>
+                    <option value="Fantascienza">Fantascienza</option>
                 </select>
             </form>
         </main>
